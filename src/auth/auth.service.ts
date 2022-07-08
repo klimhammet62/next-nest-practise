035974f2242e7c1de0d408a4622d3bc9c2162926
Belarty <belkinartemiy.dev@gmail.com>
2022-07-08 15:32:13 +0300

    auth and create token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,19 @@
 import { UserModel } from './../user/user.model';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from 'nestjs-typegoose';
 import { ModelType } from '@typegoose/typegoose/lib/types';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
-  ) {}
+	constructor(
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+	) {}
 
-  async register(dto: any) {
-    const newUser = new this.UserModel(dto);
-    return newUser.save();
-  }
+	async register(dto: AuthDto) {
+		const oldUser = this.UserModel.findOne({ email: dto.email });
+    if(oldUser) throw new BadRequestException('User with this email already registered')
+		const newUser = new this.UserModel(dto);
+		return newUser.save();
+	}
 }
