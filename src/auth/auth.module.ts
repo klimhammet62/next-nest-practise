74ce40ec9fc4549b409e7d0189e204d4c2440429
Belarty <belkinartemiy.dev@gmail.com>
2022-07-08 22:23:29 +0300

    modified:   src/auth/auth.module.ts 	modified:   src/auth/auth.service.ts 	new file:   src/auth/strategies/jwt.strategy.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,23 +1,31 @@
-import { ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModel } from 'src/user/user.model';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from 'src/config/jwt.config';
 
 @Module({
-  providers: [AuthService],
-  imports: [
-    TypegooseModule.forFeature([
-      {
-        typegooseClass: UserModel,
-        schemaOptions: {
-          collection: 'registered-users',
-        },
-      },
-    ]),
-    ConfigModule,
-  ],
-  controllers: [AuthController],
+	providers: [AuthService],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserModel,
+				schemaOptions: {
+					collection: 'registered-users',
+				},
+			},
+		]),
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig,
+		}),
+	],
+	controllers: [AuthController, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,31 @@
 import { UserModel } from './../user/user.model';
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from 'nestjs-typegoose';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { AuthDto } from './dto/auth.dto';
 import { genSalt, hash, compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 	constructor(
-		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+		private readonly jwtService: JwtService
 	) {}
 
 	async login(dto: AuthDto) {
-		return this.validateUser(dto);
+		const user = await this.validateUser(dto);
+
+		const tokens = await this.issueTokenPair(String(user._id));
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		};
 	}
 
 	async register(dto: AuthDto) {
@@ -28,16 +41,43 @@ export class AuthService {
 			password: await hash(dto.password, salt),
 		});
 
-		return newUser.save();
+		const tokens = await this.issueTokenPair(String(newUser._id));
+
+		return {
+			user: this.returnUserFields(newUser),
+			...tokens,
+		};
 	}
 
 	async validateUser(dto: AuthDto): Promise<UserModel> {
 		const user = await this.UserModel.findOne({ email: dto.email });
-		if(!user) throw new UnauthorizedException('User not found =(')
+		if (!user) throw new UnauthorizedException('User not found =(');
 
-		const isValidPassword = await compare(dto.password, user.password)
-		if(!isValidPassword) throw new UnauthorizedException('Invalid password =(')
+		const isValidPassword = await compare(dto.password, user.password);
+		if (!isValidPassword)
+			throw new UnauthorizedException('Invalid password =(');
 
-		return user
+		return user;
+	}
+
+	async issueTokenPair(userId: string) {
+		const data = { _id: userId };
+
+		const refreshToken = await this.jwtService.signAsync(data, {
+			expiresIn: '15d',
+		});
+		const accessToken = await this.jwtService.signAsync(data, {
+			expiresIn: '30m',
+		});
+
+		return { accessToken, refreshToken };
+	}
+
+	returnUserFields(user: UserModel) {
+		return {
+			_id: user._id,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		};
 	}
 }
