1aef4e87296e7f76849237e9fd91e5b03d36631b
Belarty <belkinartemiy.dev@gmail.com>
2022-07-08 14:46:35 +0300

    pls push

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,8 +5,12 @@ import { ModelType } from '@typegoose/typegoose/lib/types';
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>){}
+  constructor(
+    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+  ) {}
+
   async register(dto: any) {
-    return this.
+    const newUser = new this.UserModel(dto);
+    return newUser.save();
   }
 }
